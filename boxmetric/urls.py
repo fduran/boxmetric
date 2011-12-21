77805464b956fe62c25f3831bdae9f06eac627da
root <root@gbox.(none)>
2011-12-21 18:30:45 +0000

    url and templates

--- boxmetric/urls.py
+++ boxmetric/urls.py
@@ -1,17 +1,12 @@
 from django.conf.urls.defaults import patterns, include, url
-
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.views.generic.simple import direct_to_template
+#from django.contrib import admin
+#admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'boxmetric.views.home', name='home'),
-    # url(r'^boxmetric/', include('boxmetric.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    #(r'^admin2/', include(admin.site.urls)),
+    #(r'^api/(?P<command>\S+)/$', 'boxmetric.app.views.api'),
+    #(r'^', 'django.views.generic.simple.redirect_to', {'url': 'http://opentelligent.com/media/prod/index.html'}),
+    (r'^', direct_to_template, {'template': 'index.html', }),
+    #(r'^', 'boxmetric.app.views.index'),
 )
