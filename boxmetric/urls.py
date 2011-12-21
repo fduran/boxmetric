fd0310f1ddee7a37f0eeb0d814ba4bf03ef54f72
Fernando <duran.fernando@gmail.com>
2011-12-21 19:12:09 +0000

    views with mongo

--- boxmetric/urls.py
+++ boxmetric/urls.py
@@ -5,7 +5,7 @@ from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('',
     #(r'^admin2/', include(admin.site.urls)),
-    #(r'^api/(?P<command>\S+)/$', 'boxmetric.app.views.api'),
+    (r'^api/(?P<command>\S+)/$', 'boxmetric.app.views.api'),
     #(r'^', 'django.views.generic.simple.redirect_to', {'url': 'http://opentelligent.com/media/prod/index.html'}),
     (r'^', direct_to_template, {'template': 'index.html', }),
     #(r'^', 'boxmetric.app.views.index'),
