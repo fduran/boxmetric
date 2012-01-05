7bf595c74f744b896f278abc54d6370e3291b48f
Fernando <duran.fernando@gmail.com>
2012-01-05 22:08:08 +0000

    new status field for user profile, South seems to be working on both ends now

--- boxmetric/app/models.py
+++ boxmetric/app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Contact(models.Model):
     def __unicode__(self):
         return self.email
@@ -10,9 +11,18 @@ class Contact(models.Model):
     count = models.PositiveIntegerField(null=True,blank=True)
     thumbnail = models.CharField(max_length=128,null=True,blank=True)
 
+
 class UserProfile(models.Model):
     def __unicode__(self):
         return self.user.username
+
+    STATUS_CHOICES = (
+        (u'0', u'First Login'),
+        (u'1', u'Active'),
+        (u'2', u'Inactive'),
+    )
+
     user = models.ForeignKey(User, unique=True)
     date_created = models.DateField(auto_now_add=True)
     is_gmail = models.BooleanField(default=True)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default=u'0')
