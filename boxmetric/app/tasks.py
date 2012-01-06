c4c429b1f0a1c428f0866de01019ce1762d341f6
Fernando <duran.fernando@gmail.com>
2012-01-06 02:04:59 +0000

    Celery task is working now

--- boxmetric/app/tasks.py
+++ boxmetric/app/tasks.py
@@ -1,6 +1,11 @@
 from celery.decorators import task
 
-@task()
+@task(name="tasks.load_contacts")
 def load_contacts(account):
-    sleep(5)
+    import logging
+    import time
+    logger = logging.getLogger('project.logging.console')
+    logger.info('FDV task starts for ' + account)
+    time.sleep(5)
+    logger.info('FDV task ends')
     return
